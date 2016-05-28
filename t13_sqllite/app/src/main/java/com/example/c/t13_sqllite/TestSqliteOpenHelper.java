bc158ec5008fc5b2e279cd024f25e032778e22cc
park <psm2901@gmail.com>
2016-05-28 14:18:05 +0900

    commit

---
+++ t13_sqllite/app/src/main/java/com/example/c/t13_sqllite/TestSqliteOpenHelper.java
@@ -0,0 +1,31 @@
+package com.example.c.t13_sqllite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by c on 2016-05-28.
+ */
+public class TestSqliteOpenHelper extends SQLiteOpenHelper {
+    public TestSqliteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE student (id INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                "name TEXT, age INTEGER, address TEXT)";
+        db.execSQL(sql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "DROP TABLE IF EXISTS student";
+        db.execSQL(sql);
+
+        onCreate(db);
+
+    }
+}

31025328163872e518cac603392b4f4907870098
sm1008 <psm2901@gmail.com>
2016-06-04 10:58:14 +0900

    service2

--- t18_service2/app/src/main/java/com/psm2901/t18_service2/MainActivity.java
+++ t18_service2/app/src/main/java/com/psm2901/t18_service2/MainActivity.java
@@ -1,13 +1,55 @@
 package com.psm2901.t18_service2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyService myService=null;
+
+    boolean isBound = false;
+    ServiceConnection conn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            isBound=true;
+            MyService.MyBinder binder = (MyService.MyBinder) service;
+            myService = binder.getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            isBound=false;
+        }
+    };
+
+    public void onBtnClick(View view){
+        int value = myService.getRandomNumber();
+        Button btn = (Button)view;
+        btn.setText("num : "+value);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this,MyService.class);
+        bindService(intent,conn,BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(isBound)
+            unbindService(conn);
     }
 }

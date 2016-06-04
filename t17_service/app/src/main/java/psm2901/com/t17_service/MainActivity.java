988cded11b8b36e6655f62f6c167e8d7d928f623
sm1008 <psm2901@gmail.com>
2016-06-04 10:18:46 +0900

    _service

--- t17_service/app/src/main/java/psm2901/com/t17_service/MainActivity.java
+++ t17_service/app/src/main/java/psm2901/com/t17_service/MainActivity.java
@@ -1,7 +1,9 @@
 package psm2901.com.t17_service;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onBtnStartClick(View view){
+        Intent intent = new Intent(this,MyService.class);
+        startService(intent);
+    }
+
+
+    public void onBtnStopClick(View view){
+        Intent intent = new Intent(this,MyService.class);
+        stopService(intent);
+    }
+
 }
--- t17_service/app/src/main/java/psm2901/com/t17_service/MyService.java
+++ t17_service/app/src/main/java/psm2901/com/t17_service/MyService.java
@@ -3,9 +3,29 @@ package psm2901.com.t17_service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
     public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("service","onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("service","onDestory");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("service","onStartCommand");
+//        return super.onStartCommand(intent, flags, startId);
+        return START_STICKY;   //START_STICKY, START_NOT_STICKY, START_REDELIVERY_INTENT(실행될때까지 계속start)
     }
 
     @Override

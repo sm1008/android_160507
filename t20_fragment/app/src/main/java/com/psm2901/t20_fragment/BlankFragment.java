8708a56648f57cbfc9937719b4b6c955683e87a6
sm1008 <psm2901@gmail.com>
2016-06-04 14:50:45 +0900

    commit

--- t20_fragment/app/src/main/java/com/psm2901/t20_fragment/BlankFragment.java
+++ t20_fragment/app/src/main/java/com/psm2901/t20_fragment/BlankFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -23,7 +25,22 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        final TextView textView = (TextView)view.findViewById(R.id.textCount);
+
+        Button btnIncrease = (Button)view.findViewById(R.id.btnIncrease);
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strValue = (String) textView.getText();
+                int value = Integer.parseInt(strValue);
+                value++;
+                textView.setText(""+value);
+            }
+        });
+
+        return view;
     }
 
 }

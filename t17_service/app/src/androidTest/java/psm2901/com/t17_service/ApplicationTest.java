544380ba37b159c96c000180efbbfa3338cc7dc1
sm1008 <psm2901@gmail.com>
2016-06-04 09:46:44 +0900

    _service

---
+++ t17_service/app/src/androidTest/java/psm2901/com/t17_service/ApplicationTest.java
@@ -0,0 +1,13 @@
+package psm2901.com.t17_service;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ t17_service/app/src/main/java/psm2901/com/t17_service/MyService.java
@@ -0,0 +1,15 @@
+package psm2901.com.t17_service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
